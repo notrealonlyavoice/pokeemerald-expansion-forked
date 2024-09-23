7c73f3b9420febc24543d1ec21ff333b3672e997
notrealonlyavoice <tsxd9955@gmail.com>
2024-09-23 22:35:17 +0800

    is this still working ahh commit
    probably broken commit but otherwise playable to end

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -78,7 +78,7 @@
 #define FLAG_UNUSED_0x041    0x41 // Unused Flag
 #define FLAG_UNUSED_0x042    0x42 // Unused Flag
 #define FLAG_UNUSED_0x043    0x43 // Unused Flag
-#define FLAG_UNUSED_0x044    0x44 // Unused Flag
+#define FLAG_respawnnpc    0x44 // Unused Flag
 #define FLAG_realitempicked   0x45 // Unused Flag
 #define FLAG_UNUSED_0x046    0x46 // Unused Flag
 #define FLAG_Dontletguyrespawn   0x47 // Unused Flag
